c62ca61f14e6eeda3e28c56c37f22bcb553664b1
Eniolaonafujabi <165755379+Eniolaonafujabi@users.noreply.github.com>
2024-10-24 12:34:44 +0100

    Setting up user and store app

--- store/models.py
+++ store/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=100,null=False, blank=False)
+    description = models.CharField(max_length=255, null=False, blank=False)
+    price = models.DecimalField(max_digits= 6,decimal_places=2,null=False, blank=False)
+    inventory = models.PositiveSmallIntegerField()
+    last_updated = models.DateTimeField(auto_now=True)
+
+class Collection(models.Model):
+    title = models.CharField(max_length=100,null=False, blank=False)
