d78bb08ae8683efb8e96e4061101c13002dea54f
Eniolaonafujabi <165755379+Eniolaonafujabi@users.noreply.github.com>
2024-10-25 13:45:15 +0100

    Setting up user and store app

--- store/models.py
+++ store/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 from django.conf import settings
-from django.db.models import ForeignKey
+
 
 
 # Create your models here.
--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
+from .models import Customer
 # Register your models here.
+
+@admin.register(Customer)
+class UserAdmin(BaseUserAdmin):
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "usable_password", "password1", "password2","first_name","last_name","email"),
+            },
+        ),
+    )
+
+# admin.site.register(Customer)
